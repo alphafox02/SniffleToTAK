4fc47d6e39609894ac10a9ce35773fc62bbdde1d
alphafox02 <44436101+alphafox02@users.noreply.github.com>
2024-10-21 07:13:08 -0400

    Update sniffletotak.py

--- sniffletotak.py
+++ sniffletotak.py
@@ -148,10 +148,10 @@ class SystemStatus:
             'event',
             version='2.0',
             uid=self.id,
-            type='a-f-G-U-C',  # Friendly Ground Unit
-            time=current_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')[:-3] + 'Z',
-            start=current_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')[:-3] + 'Z',
-            stale=stale_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ')[:-3] + 'Z',
+            type='a-f-G-U-C',
+            time=current_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            start=current_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
+            stale=stale_time.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
             how='m-g'
         )
 
@@ -161,8 +161,8 @@ class SystemStatus:
             lat=str(self.lat),
             lon=str(self.lon),
             hae=str(self.alt),
-            ce='10.0',
-            le='10.0'
+            ce='35.0',
+            le='35.0'
         )
 
         detail = etree.SubElement(event, 'detail')
@@ -170,12 +170,22 @@ class SystemStatus:
         # Include contact information
         etree.SubElement(detail, 'contact', callsign=self.id)
 
+        # Include remarks early in the detail element
+        remarks_element = etree.SubElement(detail, 'remarks')
+        remarks_element.text = etree.CDATA(self.remarks)
+
+        # Include UID with Droid attribute
+        etree.SubElement(detail, 'uid', Droid=self.id)
+
+        # Include group information
+        etree.SubElement(detail, '__group', name='CIV', role='Team Member')
+
         # Include precision location
         etree.SubElement(detail, 'precisionlocation', geopointsrc='GPS', altsrc='GPS')
 
         # Include status
         status = etree.SubElement(detail, 'status')
-        status.set('battery', '100')  # Placeholder value
+        status.set('battery', '100')
         status.set('readiness', 'Available')
 
         # Include TAK version info
@@ -184,9 +194,8 @@ class SystemStatus:
         # Include track info
         etree.SubElement(detail, 'track', speed='0', course='0')
 
-        # Include color
-        etree.SubElement(detail, 'color', argb='-1')  # White color
-
+        # Include color and icon
+        etree.SubElement(detail, 'color', argb='-256')
         # Omit usericon to use the default dot icon
 
         return etree.tostring(event, pretty_print=True, xml_declaration=True, encoding='UTF-8')
